546b2e7f554841221208fdac45b8bf96eaed0142
peterataylor <peterataylor@gmail.com>
2015-08-04 10:30:05 +1000

    Adding observation schema

--- draft4/app/controllers/ValidatorController.js
+++ draft4/app/controllers/ValidatorController.js
@@ -16,11 +16,15 @@ app.controller('validatorController', function ($scope, $http, $window) {
     });
 
     $http.get('http://raw.githubusercontent.com/peterataylor/om-json/master/Common.json').success(function (data) {
-      tv4.addSchema('http://raw.githubusercontent.com/peterataylor/om-json/master/Common.json', data);
-  });
+        tv4.addSchema('http://raw.githubusercontent.com/peterataylor/om-json/master/Common.json', data);
+    });
+
+    $http.get('http://raw.githubusercontent.com/peterataylor/om-json/master/Observation.json').success(function (data) {
+        tv4.addSchema('http://raw.githubusercontent.com/peterataylor/om-json/master/Observation.json', data);
+    });
 
     $http.get('http://gist.githubusercontent.com/peterataylor/47bcd797e0ef3ac92341/raw/fb6e7428791e9afa6c8c05c5be1f5a20c8e043c1/geometry.json').success(function (data) {
-      tv4.addSchema('http://gist.githubusercontent.com/peterataylor/47bcd797e0ef3ac92341/raw/fb6e7428791e9afa6c8c05c5be1f5a20c8e043c1/geometry.json', data);
+        tv4.addSchema('http://gist.githubusercontent.com/peterataylor/47bcd797e0ef3ac92341/raw/fb6e7428791e9afa6c8c05c5be1f5a20c8e043c1/geometry.json', data);
     });
 
     this.reset = function() {
@@ -81,83 +85,83 @@ app.controller('validatorController', function ($scope, $http, $window) {
     };
 
     this.validateDocument = function () {
-      
-        console.debug("document");
-        self.documentErrors = [];
-        self.documentMessage = "";
 
-        // Parse as JSON
-        try {
-          self.documentObject = this.parseMarkup(self.document);
+      console.debug("document");
+      self.documentErrors = [];
+      self.documentMessage = "";
 
-          var valid = tv4.validate(self.documentObject, this.schemaObject, true, true);
-          if (!valid) {
-            if (tv4.error) {
-              self.documentErrors = [ {"message": tv4.error.message, 
-                  "field": tv4.error.schemaPath,
-                  "value": tv4.error.dataPath}];
-            }
-            if (tv4.missing.length != 0) {
-              self.documentErrors = [ {"message": "missing remote schema: " + tv4.missing[0], 
-                  "field": "$ref",
-                  "value": tv4.missing[0]
-              }];
-            }
-          } else {
-            self.documentMessage = "Document conforms to the JSON schema.";
+      // Parse as JSON
+      try {
+        self.documentObject = this.parseMarkup(self.document);
+
+        var valid = tv4.validate(self.documentObject, this.schemaObject, true, true);
+        if (!valid) {
+          if (tv4.error) {
+            self.documentErrors = [ {"message": tv4.error.message, 
+                "field": tv4.error.schemaPath,
+                "value": tv4.error.dataPath}];
           }
-
-        } catch (e) {
-          // Error parsing as JSON
-          self.documentErrors = [{message: "Document is invalid JSON. Try http://jsonlint.com to fix it." }];
+          if (tv4.missing.length != 0) {
+            self.documentErrors = [ {"message": "missing remote schema: " + tv4.missing[0], 
+                "field": "$ref",
+                "value": tv4.missing[0]
+            }];
+          }
+        } else {
+          self.documentMessage = "Document conforms to the JSON schema.";
         }
-  };
 
-  this.validateSchema = function () {
-    console.debug("schema");
-    self.schemaErrors = [];
-    self.schemaMessage = "";
+      } catch (e) {
+        // Error parsing as JSON
+        self.documentErrors = [{message: "Document is invalid JSON. Try http://jsonlint.com to fix it." }];
+      }
+    };
 
-    // Parse as JSON
-    try {
-      self.schemaObject = this.parseMarkup(self.schema);
+    this.validateSchema = function () {
+      console.debug("schema");
+      self.schemaErrors = [];
+      self.schemaMessage = "";
 
-      // Can't be done if we don't have the meta schema yet
-      if (!this.metaSchema) {
-        return;
+      // Parse as JSON
+      try {
+        self.schemaObject = this.parseMarkup(self.schema);
+
+        // Can't be done if we don't have the meta schema yet
+        if (!this.metaSchema) {
+          return;
+        }
+
+        // Do validation
+        var schemaValidator = validator(this.metaSchema, {
+            verbose: true
+        });
+        schemaValidator(this.schemaObject);
+        console.log(schemaValidator.errors)
+        if (schemaValidator.errors && schemaValidator.errors.length) {
+          this.schemaErrors = schemaValidator.errors;
+        } else {
+          this.schemaMessage = "Schema is a valid JSON schema.";
+        }
+      } catch (e) {
+        // Error parsing as JSON
+        self.schemaErrors = [{ message: "Schema is invalid JSON. Try http://jsonlint.com to fix it." }];
       }
 
-      // Do validation
-      var schemaValidator = validator(this.metaSchema, {
-          verbose: true
-      });
-      schemaValidator(this.schemaObject);
-      console.log(schemaValidator.errors)
-      if (schemaValidator.errors && schemaValidator.errors.length) {
-        this.schemaErrors = schemaValidator.errors;
-      } else {
-        this.schemaMessage = "Schema is a valid JSON schema.";
-      }
-    } catch (e) {
-      // Error parsing as JSON
-      self.schemaErrors = [{ message: "Schema is invalid JSON. Try http://jsonlint.com to fix it." }];
-    }
+    };
 
-  };
+    // Document changes
+    $scope.$watch(function () {
+        return self.document;
+      }, function (newValue, oldValue) {
+        self.validateDocument();
+    });
 
-  // Document changes
-  $scope.$watch(function () {
-      return self.document;
-    }, function (newValue, oldValue) {
-      self.validateDocument();
-  });
-
-  // Schema changes
-  $scope.$watch(function () {
-      return self.schema;
-    }, function (newValue, oldValue) {
-      self.validateSchema();
-      self.validateDocument();
-  });
+    // Schema changes
+    $scope.$watch(function () {
+        return self.schema;
+      }, function (newValue, oldValue) {
+        self.validateSchema();
+        self.validateDocument();
+    });
 
 });
